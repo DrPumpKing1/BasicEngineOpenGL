0f8d03b91a201fecf6fa70a87f5bc07dcb7104d2
Salvador Rodriguez <salvadorcreativo429@gmail.com>
2026-02-17 00:11:46 -0500

    Parallax Mapping and Normal mapping
    with Gamma correction for rendering a plane

--- resources/shaders/shader.fs
+++ resources/shaders/shader.fs
@@ -4,24 +4,80 @@ out vec4 FragColor;
 in VS_OUT {
     vec3 FragPos;
     vec2 TexCoords;
+    vec3 TangentLightPos;
+    vec3 TangentViewPos;
+    vec3 TangentFragPos;
 } fs_in;
 
-vec3 lightDirection = vec3(0.0f, 0.5f, 5.0f);
+float constant = 1.0;
+float linear = 0.7;
+float quadratic = 1.8;
 
 vec3 ambientColor = vec3(0.1, 0.1, 0.1);
 vec3 diffuseColor = vec3(0.9, 0.9, 0.9);
+vec3 specularColor = vec3(1.0, 1.0, 1.0);
+
+float shininess = 32.0;
+
+float heightScale = 0.1;
 
 uniform sampler2D diffuseTexture;
 uniform sampler2D normalTexture;
+uniform sampler2D depthTexture;
+
+vec2 ParallaxMapping(vec2 texCoords, vec3 viewDirection) 
+{
+    const float minLayers = 8.0;
+    const float maxLayers = 32.0;
+    float numLayers = mix(maxLayers, minLayers, max(dot(vec3(0.0, 0.0, 1.0), viewDirection), 0.0));
+    float layerDepth = 1.0 / numLayers;
+    float currentLayerDepth = 0.0;
+
+    vec2 approximationDirection = viewDirection.xy * heightScale; 
+    vec2 deltaTexCoords = approximationDirection / numLayers;
+
+    vec2  currentTexCoords     = texCoords;
+    float currentDepthMapValue = texture(depthTexture, currentTexCoords).r;
+  
+    while(currentLayerDepth < currentDepthMapValue)
+    {
+        currentTexCoords -= deltaTexCoords;
+        currentDepthMapValue = texture(depthTexture, currentTexCoords).r;  
+        currentLayerDepth += layerDepth;  
+    }
+
+    vec2 prevTexCoords = currentTexCoords + deltaTexCoords;
+
+    float afterDepth  = currentDepthMapValue - currentLayerDepth;
+    float beforeDepth = texture(depthTexture, prevTexCoords).r - currentLayerDepth + layerDepth;
+ 
+    float weight = afterDepth / (afterDepth - beforeDepth);
+    vec2 finalTexCoords = prevTexCoords * weight + currentTexCoords * (1.0 - weight);
+
+    return finalTexCoords;
+}
 
 void main() 
 {
-    vec3 diffuseSample = texture(diffuseTexture, fs_in.TexCoords).rgb;
+    vec3 viewDirection = normalize(fs_in.TangentViewPos - fs_in.TangentFragPos);
 
-    vec3 normalDirection = texture(normalTexture, fs_in.TexCoords).rgb * 2.0 - 1.0;
+    vec2 texCoords = ParallaxMapping(fs_in.TexCoords,  viewDirection);
+    if(texCoords.x > 1.0 || texCoords.y > 1.0 || texCoords.x < 0.0 || texCoords.y < 0.0)
+        discard;
+
+    vec3 diffuseSample = texture(diffuseTexture, texCoords).rgb;
+    vec3 normalSample = texture(normalTexture, texCoords).rgb * 2.0 - 1.0;
+
+    vec3 normalDirection = normalize(normalSample);
+    vec3 lightDirection = normalize(fs_in.TangentLightPos - fs_in.TangentFragPos);
+    vec3 halfwayDirection = normalize(lightDirection + viewDirection);
 
     float diffuse = max(dot(normalDirection, lightDirection), 0.0);
+    float specular = pow(max(dot(normalDirection, halfwayDirection), 0.0), shininess);
 
-    vec3 result = (ambientColor + diffuseColor * diffuse) * diffuseSample;
+    float distance = length(fs_in.TangentLightPos - fs_in.TangentFragPos);
+    float attenuation = 1.0 / (constant + linear * distance + quadratic * (distance * distance));
+
+    vec3 result = ((ambientColor + diffuseColor * diffuse) * diffuseSample + specular * specularColor ) * attenuation;
     FragColor = vec4(result, 1.0);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,7 @@
 #include "Camera/Camera.h"
 #include "Shader/ShaderProgram.h"
 #include "Texture/Texture.h"
+#include "Vertex/Vertex.h"
 
 const unsigned int WIDTH {800};
 const unsigned int HEIGHT {600};
@@ -21,12 +22,14 @@ void CursorCallback(GLFWwindow* window, double xposIn, double yposIn);
 void ScrollCallback(GLFWwindow* window, double xoffset, double yoffset);
 void ProcessInput(GLFWwindow* window);
 
-float quadVertices[] = {
-    // positions        // texture Coords
-    -1.0f,  1.0f, 0.0f, 0.0f, 1.0f,
-    -1.0f, -1.0f, 0.0f, 0.0f, 0.0f,
-     1.0f,  1.0f, 0.0f, 1.0f, 1.0f,
-     1.0f, -1.0f, 0.0f, 1.0f, 0.0f,
+Vertex vertices[] = {
+    // positions          // texture coords  // normals           // tangents
+    {{-1.0f,  1.0f, 0.0f}, {0.0f, 1.0f}, {0.0f, 0.0f, 1.0f}, {0.0f, 0.0f, 0.0f}},
+    {{-1.0f, -1.0f, 0.0f}, {0.0f, 0.0f}, {0.0f, 0.0f, 1.0f}, {0.0f, 0.0f, 0.0f}},
+    {{ 1.0f,  1.0f, 0.0f}, {1.0f, 1.0f}, {0.0f, 0.0f, 1.0f}, {0.0f, 0.0f, 0.0f}},
+    {{-1.0f, -1.0f, 0.0f}, {0.0f, 0.0f}, {0.0f, 0.0f, 1.0f}, {0.0f, 0.0f, 0.0f}},
+    {{ 1.0f, -1.0f, 0.0f}, {1.0f, 0.0f}, {0.0f, 0.0f, 1.0f}, {0.0f, 0.0f, 0.0f}},
+    {{ 1.0f,  1.0f, 0.0f}, {1.0f, 1.0f}, {0.0f, 0.0f, 1.0f}, {0.0f, 0.0f, 0.0f}},
 };
 
 Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
@@ -37,6 +40,8 @@ float lastTime{0.0f};
 float lastCursorX{WIDTH / 2.0f};
 float lastCursorY{HEIGHT / 2.0f};
 bool firstCursorClick{true};
+
+glm::vec3 lightPosition(0.0f, 1.0f, 0.5f);
 
 int main( void)
 {
@@ -53,6 +58,8 @@ int main( void)
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 #endif
 
+    glfwWindowHint(GLFW_SAMPLES, 4);
+
     GLFWwindow *window = glfwCreateWindow(currentWidth, currentHeight, "Basic OpenGL Window", nullptr, nullptr);
     if(window == nullptr) {
         std::cerr << "Failed to create GLFW window" << std::endl;
@@ -64,12 +71,32 @@ int main( void)
     glfwSetFramebufferSizeCallback(window, FramebufferSizeCallback);
     glfwSetCursorPosCallback(window, CursorCallback);
     glfwSetScrollCallback(window, ScrollCallback);
-    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
 
     if(!gladLoadGL((GLADloadfunc)glfwGetProcAddress)) {
         std::cerr << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
+
+    glm::vec3 edge1 = vertices[1].position - vertices[0].position;
+    glm::vec3 edge2 = vertices[2].position - vertices[0].position;
+    glm::vec2 deltaUV1 = vertices[1].texCoords - vertices[0].texCoords;
+    glm::vec2 deltaUV2 = vertices[2].texCoords - vertices[0].texCoords;
+    float f = 1.0f / (deltaUV1.x * deltaUV2.y - deltaUV2.x * deltaUV1.y);
+
+    vertices[0].tangent = f * (deltaUV2.y * edge1 - deltaUV1.y * edge2);
+    vertices[1].tangent = vertices[0].tangent;
+    vertices[2].tangent = vertices[0].tangent;
+
+    edge1 = vertices[4].position - vertices[3].position;
+    edge2 = vertices[5].position - vertices[3].position;
+    deltaUV1 = vertices[4].texCoords - vertices[3].texCoords;
+    deltaUV2 = vertices[5].texCoords - vertices[3].texCoords;
+    f = 1.0f / (deltaUV1.x * deltaUV2.y - deltaUV2.x * deltaUV1.y);
+
+    vertices[3].tangent = f * (deltaUV2.y * edge1 - deltaUV1.y * edge2);
+    vertices[4].tangent = vertices[3].tangent;
+    vertices[5].tangent = vertices[3].tangent;
 
     GLuint VBO, VAO;
     glGenVertexArrays(1, &VAO);
@@ -77,25 +104,40 @@ int main( void)
 
     glBindVertexArray(VAO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), quadVertices, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
     glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, position));
     glEnableVertexAttribArray(1);
-    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(sizeof(float) * 3));
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, texCoords));
+    glEnableVertexAttribArray(2);
+    glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, normal));
+    glEnableVertexAttribArray(3);
+    glVertexAttribPointer(3, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, tangent));
 
     glBindVertexArray(0);
     glBindBuffer(GL_ARRAY_BUFFER, 0);
 
-    Shader vertexShader("shader.vs", ShaderType::VERTEX);
-    Shader fragmentShader("shader.fs", ShaderType::FRAGMENT);
-    ShaderProgram shaderProgram(std::vector<Shader>{vertexShader, fragmentShader});
+    Shader *vertexShader = new Shader("shader.vs", ShaderType::VERTEX);
+    Shader *fragmentShader = new Shader("shader.fs", ShaderType::FRAGMENT);
+    ShaderProgram *shaderProgram = new ShaderProgram(std::vector<Shader>{*vertexShader, *fragmentShader});
 
-    Texture diffuseTexture("resources/textures/brickwall.jpg", TextureType::DIFFUSE, GL_TEXTURE0);
-    Texture normalTexture("resources/textures/brickwall_normal.jpg", TextureType::NORMAL, GL_TEXTURE1);
+    delete vertexShader;
+    delete fragmentShader;
 
-    shaderProgram.Bind();
-    diffuseTexture.SetShaderUniform(shaderProgram, "diffuseTexture");
-    normalTexture.SetShaderUniform(shaderProgram, "normalTexture");
+    Texture *diffuseTexture = new Texture("resources/textures/bricks2.jpg", TextureType::DIFFUSE, GL_TEXTURE0);
+    Texture *normalTexture = new Texture("resources/textures/bricks2_normal.jpg", TextureType::NORMAL, GL_TEXTURE1);
+    Texture *depthTexture = new Texture("resources/textures/bricks2_disp.jpg", TextureType::DISPLACEMENT, GL_TEXTURE2);
+
+    shaderProgram->Bind();
+    diffuseTexture->SetShaderUniform(*shaderProgram, "diffuseTexture");
+    normalTexture->SetShaderUniform(*shaderProgram, "normalTexture");
+    depthTexture->SetShaderUniform(*shaderProgram, "depthTexture");
+
+    shaderProgram->SetVec3("lightPosition", lightPosition);
+
+    glEnable(GL_MULTISAMPLE);
+    glEnable(GL_FRAMEBUFFER_SRGB);
+    glEnable(GL_CULL_FACE);
 
     while(!glfwWindowShouldClose(window)) {
         float currentTime = static_cast<float>(glfwGetTime());
@@ -107,34 +149,44 @@ int main( void)
         glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        shaderProgram.Bind();
-        shaderProgram.SetMat4("view", camera.GetViewMatrix());
+        shaderProgram->Bind();
+        shaderProgram->SetMat4("view", camera.GetViewMatrix());
 
         glm::mat4 projection = glm::perspective(glm::radians(camera.zoom), static_cast<float>(currentWidth) / currentHeight, 0.1f, 100.0f);
-        shaderProgram.SetMat4("projection", projection);
+        shaderProgram->SetMat4("projection", projection);
 
         glm::mat4 model = glm::mat4(1.0f);
-        shaderProgram.SetMat4("model", model);
+        model = glm::rotate(model, currentTime * glm::radians(-10.0f), glm::vec3(1.0f, 0.0f, 1.0f));
+        shaderProgram->SetMat4("model", model);
 
-        diffuseTexture.Bind();
-        normalTexture.Bind();
+        shaderProgram->SetVec3("viewPosition", camera.position);
+
+        diffuseTexture->Bind();
+        normalTexture->Bind();
+        depthTexture->Bind();
 
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+        glDrawArrays(GL_TRIANGLES, 0, sizeof(vertices) / sizeof(Vertex));
         glBindVertexArray(0);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
+    glDeleteBuffers(1, &VBO);
+    glDeleteVertexArrays(1, &VAO);
+
+    delete shaderProgram;
+
+    delete diffuseTexture;
+    delete normalTexture;
+    delete depthTexture;
+
     glfwTerminate();
     return 0;
 }
 
 void ProcessInput(GLFWwindow* window) {
-    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, true);
-
     if(glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS)
         camera.ProcessKeyboardSpeed(true);
     else
